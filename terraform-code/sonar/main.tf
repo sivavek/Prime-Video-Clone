2d6aeee1dee7b2783050be01db54e99d99f52d0f
sivavek <sivakumarvivek.devops@gmail.com>
2025-11-15 14:18:54 +0530

    force delete added

--- terraform-code/sonar/main.tf
+++ terraform-code/sonar/main.tf
@@ -19,6 +19,8 @@ resource "aws_ecr_repository" "app_repo" {
   name                 = var.ecr_repo_name
   image_tag_mutability = "MUTABLE"
 
+  force_delete = true
+
   image_scanning_configuration {
     scan_on_push = true
   }
