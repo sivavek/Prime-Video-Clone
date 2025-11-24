de906567d1cb66fdadb20d77e19458f339212869
sivavek <sivakumarvivek.devops@gmail.com>
2025-11-24 15:31:48 +0530

    changed infra code

---
+++ terraform-code/eks-infra/eks.tf
@@ -0,0 +1,81 @@
+locals {
+  region = "ap-south-1"
+  name   = "prime-video-clone-eks-cluster"
+  vpc_cidr = "10.0.0.0/16"
+  azs      = ["ap-south-1a", "ap-south-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
+  tags = {
+    Example = local.name
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.13.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = local.private_subnets
+  public_subnets  = local.public_subnets
+  intra_subnets   = local.intra_subnets
+
+  enable_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.1"
+
+  cluster_name                   = local.name
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    poc18-node = {
+      min_size     = 2
+      max_size     = 4
+      desired_size = 2
+
+      instance_types = ["t2.medium"]
+      capacity_type  = "SPOT"
+
+      tags = {
+        ExtraTag = "poc18-node-group"
+      }
+    }
+  }
+
+  tags = local.tags
+}
+
