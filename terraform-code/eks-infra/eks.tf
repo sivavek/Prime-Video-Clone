981858ae1d5333c522e2f2fb975ed446b8cf15e6
sivavek <sivakumarvivek.devops@gmail.com>
2025-11-29 00:43:33 +0530

    update

--- terraform-code/eks-infra/eks.tf
+++ terraform-code/eks-infra/eks.tf
@@ -1,6 +1,6 @@
 locals {
   region = "ap-south-1"
-  name   = "prime-video-clone-eks-cluster"
+  name   = "prime-video-eks-cluster"
   vpc_cidr = "10.0.0.0/16"
   azs      = ["ap-south-1a", "ap-south-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
